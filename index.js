79aba7cad26aa9903107155d0bbb6cfc656a2922
Jake Posch <jposch001@hotmail.com>
2021-11-11 12:03:03 -0600

    node js training

--- index.js
+++
@@ -1,7 +0,0 @@
-var date = new Date();
-
-var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-
-exports.dayOfWeek = function () {
-    return days[ date.getDate() - 1];
-};
